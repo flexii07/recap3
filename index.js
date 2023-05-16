d686945a715a9704ff9aca65193dfd9b697184c2
Anastasiia Chetverykova <nastiche@Anastasiias-Air.heiningair.local>
2023-05-16 14:05:42 +0200

    add pagination

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import { createCharacterCard } from "./components/card/card.js";
-
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -9,19 +8,21 @@ const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
-
 // States
-const maxPage = 1;
-const page = 1;
+let maxPage = 1;
+let page = 1;
 const searchQuery = "";
-
 async function fetchCharacters() {
   try {
-    const response = await fetch("https://rickandmortyapi.com/api/character");
+    const response = await fetch(
+      `https://rickandmortyapi.com/api/character?page=${page}`
+    );
     if (response.ok) {
       const data = await response.json();
+      maxPage = data.info.pages;
       console.log(data.results);
       // const characters = data.results;
+      cardContainer.innerHTML = "";
       data.results.forEach((character) => {
         const newCharacter = createCharacterCard(
           character.image,
@@ -33,9 +34,25 @@ async function fetchCharacters() {
         console.log(character);
         cardContainer.append(newCharacter);
       });
+      updatePageNumbers();
     }
   } catch (error) {
     console.error("An error occurred");
   }
+}
+nextButton.addEventListener("click", () => {
+  if (page < maxPage) {
+    page++;
+    fetchCharacters();
+  }
+});
+prevButton.addEventListener("click", () => {
+  if (page > 1) {
+    page--;
+    fetchCharacters();
+  }
+});
+function updatePageNumbers() {
+  pagination.textContent = `${page} / ${maxPage}`;
 }
 fetchCharacters();
